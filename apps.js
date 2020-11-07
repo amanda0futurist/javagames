9fffe9df79cf8cd217d81f24491989b699b987bd
amanda0futurist <eamandabox@gmail.com>
2020-11-06 18:47:45 -0600

    progress

--- apps.js
+++ apps.js
@@ -2,10 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50
-    let doodlerBottomSpace = 150
+    let doodlerBottomSpace = 250
     let isGameOver = false
     let platformCount = 5
     let platforms = []
+    let upTimerId
+    let downTimerId
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -33,22 +35,42 @@ document.addEventListener('DOMContentLoaded', () => {
             let newPlatBottom = 100 + i * platGap
             let newPlatform = new Platform(newPlatBottom)
             platforms.push(newPlatform)
-            console.log(platforms)
         }
     }
     function movePlatforms (){
         if (doodlerBottomSpace > 200){
             platforms.forEach(platform => {
                 platform.bottom -= 4
+                let visual = platform.visual
+                visual.style.bottom = platform.bottom + 'px'
             })
         }
+    }
+
+    function jump() {
+        clearInterval(downTimerId)
+        upTimerId = setInterval(function () {
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace > 350) {
+                fall()
+            }
+        },30)
+    }
+
+    function fall(){
+        clearInterval(upTimerId)
+        downTimerId = setInterval(function(){
+            doodlerBottomSpace -= 5
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+        },30)
     }
 
     function start() {
         if (!isGameOver){
             createDoodler()
             createPlatforms()
-            movePlatforms()
+            setInterval(movePlatforms, 30)
         }
     }
     start()
